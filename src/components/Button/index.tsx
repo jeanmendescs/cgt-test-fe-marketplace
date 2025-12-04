179159a3e551f30f4ecb2fc6a09cd7541b141f2c
Jean <mendes.jean.cs@gmail.com>
2025-12-04 16:14:19 -0300

    Updates

--- src/components/Button/index.tsx
+++
@@ -1,7 +0,0 @@
-import OutlinedButton from "./Variants/Outlined/OutlinedButton";
-import ContainedButton from "./Variants/Contained/ContainedButton";
-
-export const Button = {
-  Outlined: OutlinedButton,
-  Contained: ContainedButton,
-};
